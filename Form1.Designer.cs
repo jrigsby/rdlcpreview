4985f631d0fb3f393de329d213d98ad698bcfc4b
John Rigsby <jrigsby@tcrdev.com>
2018-08-29 10:47:22 -0400

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ReportsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+         this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+         this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+         this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.assignDataFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.assignReportFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+         this.menuStrip1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // reportViewer1
+         // 
+         this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.reportViewer1.Location = new System.Drawing.Point(0, 24);
+         this.reportViewer1.Name = "reportViewer1";
+         this.reportViewer1.ServerReport.BearerToken = null;
+         this.reportViewer1.Size = new System.Drawing.Size(682, 362);
+         this.reportViewer1.TabIndex = 0;
+         // 
+         // menuStrip1
+         // 
+         this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.reloadToolStripMenuItem});
+         this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+         this.menuStrip1.Name = "menuStrip1";
+         this.menuStrip1.Size = new System.Drawing.Size(682, 24);
+         this.menuStrip1.TabIndex = 1;
+         this.menuStrip1.Text = "menuStrip1";
+         // 
+         // reloadToolStripMenuItem
+         // 
+         this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+         this.reloadToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+         this.reloadToolStripMenuItem.Text = "Reload";
+         this.reloadToolStripMenuItem.Click += new System.EventHandler(this.reloadToolStripMenuItem_Click);
+         // 
+         // fileToolStripMenuItem
+         // 
+         this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.assignDataFileToolStripMenuItem,
+            this.assignReportFileToolStripMenuItem});
+         this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+         this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+         this.fileToolStripMenuItem.Text = "File";
+         // 
+         // assignDataFileToolStripMenuItem
+         // 
+         this.assignDataFileToolStripMenuItem.Name = "assignDataFileToolStripMenuItem";
+         this.assignDataFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+         this.assignDataFileToolStripMenuItem.Text = "Assign &Data File";
+         this.assignDataFileToolStripMenuItem.Click += new System.EventHandler(this.assignDataFileToolStripMenuItem_Click);
+         // 
+         // assignReportFileToolStripMenuItem
+         // 
+         this.assignReportFileToolStripMenuItem.Name = "assignReportFileToolStripMenuItem";
+         this.assignReportFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+         this.assignReportFileToolStripMenuItem.Text = "Assign &Report File";
+         this.assignReportFileToolStripMenuItem.Click += new System.EventHandler(this.assignReportFileToolStripMenuItem_Click);
+         // 
+         // openFileDialog1
+         // 
+         this.openFileDialog1.FileName = "openFileDialog1";
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(682, 386);
+         this.Controls.Add(this.reportViewer1);
+         this.Controls.Add(this.menuStrip1);
+         this.MainMenuStrip = this.menuStrip1;
+         this.Name = "Form1";
+         this.Text = "RDCL Preview";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         this.menuStrip1.ResumeLayout(false);
+         this.menuStrip1.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+      private System.Windows.Forms.MenuStrip menuStrip1;
+      private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem assignDataFileToolStripMenuItem;
+      private System.Windows.Forms.ToolStripMenuItem assignReportFileToolStripMenuItem;
+      private System.Windows.Forms.OpenFileDialog openFileDialog1;
+   }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml;
+using Microsoft.Reporting.WinForms;
+
+namespace ReportsApplication1 {
+   public partial class Form1 : Form {
+      private readonly string _folder = AppDomain.CurrentDomain.BaseDirectory;
+      private string _lastReportFolder = AppDomain.CurrentDomain.BaseDirectory;
+      private string _lastDataFolder = AppDomain.CurrentDomain.BaseDirectory;
+      private string _dataPath = "";
+      private string _reportPath = "";
+      public Form1() {
+         InitializeComponent();
+      }
+
+      private void Form1_Load(object sender, EventArgs e) {
+         openFileDialog1.InitialDirectory = _folder;
+         openFileDialog1.FileName = "";
+         openFileDialog1.Multiselect = false;
+
+         LoadReport();
+         reportViewer1.RefreshReport();
+      }
+
+      #region Menu Events
+      private void reloadToolStripMenuItem_Click(object sender, EventArgs e) {
+         LoadReport();
+      }
+
+      private void assignDataFileToolStripMenuItem_Click(object sender, EventArgs e) {
+         SetDataPath();
+      }
+
+      private void assignReportFileToolStripMenuItem_Click(object sender, EventArgs e) {
+         SetReportPath();
+      }
+      #endregion
+
+      #region Private methods
+      private void LoadReport() {
+         if (string.IsNullOrEmpty(_dataPath)) {
+            SetDataPath();
+            return;
+         }
+         if (string.IsNullOrEmpty(_reportPath)) {
+            SetReportPath();
+            return;
+         }
+
+         var ds = new DataSet();
+         ds.ReadXml(_dataPath, XmlReadMode.ReadSchema);
+
+         XmlDocument xml = new XmlDocument();
+         xml.Load(_reportPath);
+
+         //add namespaces so that xml may be traversed
+         //http://microsoft.public.sqlserver.reportingsvcs.narkive.com/U1JHd8Nj/unable-to-parse-rdlc-with-xpath
+         var ns = new XmlNamespaceManager(xml.NameTable);
+         ns.AddNamespace("ns", "http://schemas.microsoft.com/sqlserver/reporting/2008/01/reportdefinition");
+         ns.AddNamespace("rd", "http://schemas.microsoft.com/SQLServer/reporting/reportdesigner");
+
+         reportViewer1.ProcessingMode = ProcessingMode.Local;
+         reportViewer1.Reset();
+         reportViewer1.LocalReport.ReportPath = _reportPath;
+         reportViewer1.LocalReport.DataSources.Clear();
+
+         //We need to add datasets used by the report as defined in the report itself.
+         //The report knows the name it uses to refer to the dataset and the table in the xml dataset its refering to
+         //We need to make that association so the user doesnt need to define them
+         try {
+            // ReSharper disable once PossibleNullReferenceException
+            foreach (XmlElement dataset in xml.SelectNodes("//ns:Report/ns:DataSets/ns:DataSet", ns)) {
+               var name = dataset.GetAttribute("Name");
+               var table = dataset.SelectSingleNode("rd:DataSetInfo/rd:TableName", ns)?.InnerText;
+               reportViewer1.LocalReport.DataSources.Add(new ReportDataSource(name, ds.Tables[table]));
+            }
+         }
+         catch (Exception ex) {
+            var msg = $"Problem loading datasets: {ex.Message}";
+            MessageBox.Show(msg);
+         }
+         reportViewer1.RefreshReport();
+      }
+
+      private void SetDataPath() {
+         openFileDialog1.Filter = @"XML Files|*.xml";
+         openFileDialog1.Title = @"Please select a data file to preview.";
+         openFileDialog1.CheckFileExists = true;
+         openFileDialog1.InitialDirectory = _lastDataFolder;
+         if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+         _dataPath = openFileDialog1.FileName;
+         var fi = new System.IO.FileInfo(openFileDialog1.FileName);
+         _lastDataFolder = fi.DirectoryName;
+         LoadReport();
+      }
+
+      private void SetReportPath() {
+         openFileDialog1.Filter = @"Client Report Definition Files|*.rdlc";
+         openFileDialog1.Title = @"Please select a report file to preview.";
+         openFileDialog1.CheckFileExists = true;
+         openFileDialog1.InitialDirectory = _lastReportFolder;
+         if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+         _reportPath = openFileDialog1.FileName;
+         var fi = new System.IO.FileInfo(openFileDialog1.FileName);
+         _lastReportFolder = fi.DirectoryName;
+         LoadReport();
+      }
+      #endregion
+
+      //private void reloadToolStripMenuItem_Click(object sender, EventArgs e) {
+      //   var folder = AppDomain.CurrentDomain.BaseDirectory;
+      //   DataSet ds = new DataSet();
+      //   ds.ReadXml("c:\\temp\\data\\data.xml", XmlReadMode.ReadSchema);
+      //   // Setup the report viewer object and get the array of bytes
+
+
+      //   this.reportViewer1.LocalReport.DataSources.Clear();
+      //   this.reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("PlanDetail", ds.Tables["PlanDetail"])); // Add datasource here
+      //   this.reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("FundData", ds.Tables["FundData"]));
+      //   this.reportViewer1.LocalReport.Refresh();
+
+      //   this.reportViewer1.RefreshReport();
+      //}
+   }
+}
